40db4ff169c79dd2abfe891631b55976a142975f
Schuster-Soliven <77858227+Schuster-Soliven@users.noreply.github.com>
2023-05-11 20:26:41 -0700

    Add files via upload

--- LargestSum.h
+++ LargestSum.h
@@ -9,10 +9,9 @@ std::vector<int> largest_sum(std::vector<int> num_vec) {
     int temp = 0;
     std::vector<int> temp_vec;
     std::vector<int> largest_vec;
-    int subArrayTotal = pow(2, n);
     for (int i = 0; i < n; i++) {
         for (int j = i; j < n; j++) {
-            for (int k = i; k <= j; k++){
+            for (int k = i; k <= j; k++) {
                 temp += num_vec[k];
                 temp_vec.push_back(num_vec[k]);
             }
