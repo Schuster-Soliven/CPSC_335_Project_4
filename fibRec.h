f051f0de65e4e7a4270b2ea46977b0fcc716ba8f
Thomas-Stanton <55109510+Thomas-Stanton@users.noreply.github.com>
2023-05-12 18:42:34 -0700

    Update fibRec.h

--- fibRec.h
+++ fibRec.h
@@ -10,9 +10,10 @@ bool valid(int val) {
   return true;
 }
 
-int fibo_helper(int prev, int current, int nth) {
-  if (nth == 1) {
-    return prev;
+int fibo_helper(int prev, int current, int nth)//worst case O(7)
+{
+  if (nth == 1) //1{
+    return prev;//1
   }
   else {
     return fibo_helper(current, current + prev, nth - 1);
@@ -22,17 +23,17 @@ int fibo_helper(int prev, int current, int nth) {
 
 /*********************************************************************8*/
 // Approximate value of golden ratio
-double PHI = 1.6180339;
+double PHI = 1.6180339;//1
 
-double fibonacciNumber(unsigned int n) {
-  int Fp = 0;
-  return Fp = ((pow(1 + sqrt(5), n) - (pow(1 - sqrt(5), n))) / (pow(2, n) * sqrt(5)));
+double fibonacciNumber(unsigned int n)//worst O(5) {
+  int Fp = 0;//1
+  return Fp = ((pow(1 + sqrt(5), n) - (pow(1 - sqrt(5), n))) / (pow(2, n) * sqrt(5)));//4
 }
 
-int nthNumber(int n, int p) {
+int nthNumber(int n, int p)//worst case O(6) {
   return fibonacciNumber(n) * pow(PHI, n - p);
 }
 
-int nextNumber(int n) {
+int nextNumber(int n) //worst case O(6){
   return fibonacciNumber( n)* PHI;
 }
